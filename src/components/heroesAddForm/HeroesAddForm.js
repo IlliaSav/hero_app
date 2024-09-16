e5d1590568155f5cc46a9e18cf68fa2dc078a4e9
Illia <examle@gmail.com>
2024-09-16 10:26:55 +0300

    Refactoring on RTK

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -1,21 +1,21 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 
-import {useHttp} from '../../hooks/http.hook';
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import store from "../../store";
-import { heroCreated } from '../heroesList/heroesSlice';
+
 import { selectAll } from '../heroesFilters/filtersSlice';
+import { useCreateHeroMutation } from '../../api/apiSlice';
 
 const HeroesAddForm = () => {
     const [heroName, setHeroName] = useState('');
     const [heroDescr, setHeroDescr] = useState('');
     const [heroElement, setHeroElement] = useState('');
 
+    const [createHero, {isLoading}] = useCreateHeroMutation();
+
     const {filtersLoadingStatus} = useSelector(state => state.filters);
     const filters = selectAll(store.getState())
-    const dispatch = useDispatch();
-    const {request} = useHttp();
 
     const onSubmitHandler = (e) => {
         e.preventDefault();
@@ -27,10 +27,7 @@ const HeroesAddForm = () => {
             element: heroElement
         }
 
-        request("http://localhost:3001/heroes", "POST", JSON.stringify(newHero))
-            .then(res => console.log(res, 'Created'))
-            .then(dispatch(heroCreated(newHero)))
-            .catch(err => console.log(err));
+        createHero(newHero).unwrap();
 
         setHeroName('');
         setHeroDescr('');
